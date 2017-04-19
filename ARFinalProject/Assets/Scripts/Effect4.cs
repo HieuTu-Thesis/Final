c4e21d5404853d03d4a968c5cc60def13ed2716a
trantuantu <oceanprince0209@gmail.com>
2017-04-20 01:24:16 +0700

    Add board

---
+++ ARFinalProject/Assets/Scripts/Effect4.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect4 : MonoBehaviour {
+    public bool _isShow;
+    public float _delayTime;
+    public GameObject _effect;
+    public GameObject _show;
+	// Use this for initialization
+	void Start () {
+        _isShow = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_isShow == true)
+        {
+            StartCoroutine(StartWait(_delayTime));
+        }
+	}
+    IEnumerator StartWait(float time)
+    {
+        yield return StartCoroutine(Wait(time));
+        _effect.SetActive(true);
+        _show.SetActive(true);
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+}
