21b90f7bc7cc1611dab1a75f71c7ca6f649bb4c5
trantuantu <oceanprince0209@gmail.com>
2017-05-09 01:22:34 +0700

    Add games, Fix effect

---
+++ ARFinalProject/Assets/Scripts/Effects/Effect2.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect2 : MonoBehaviour {
+    public GameObject[] _gameObjects;
+
+    public float R, G, B;
+    public int _endY;
+    public float _delayTime;
+    private int _endIdx, _startIdx, y, _originY;
+    private int _idx;
+
+    public float _degrees, _changeDegrees;
+    private int[] _count;
+    public bool _rotateEffect;
+    public bool _initEffect;
+    public bool _isStartRotate;
+    // Use this for initialization
+    void Start () {
+        _isStartRotate = false;
+        _endIdx = 1;
+        _startIdx = 0;
+        _originY = (int)_gameObjects[0].transform.localPosition.y;
+        _idx = 0;
+        y = (int)(0.7F * _originY);
+
+        _count = new int[_gameObjects.Length];
+        for (int i = 0; i < _count.Length; i++)
+            _count[i] = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (_initEffect == true && _startIdx != _endIdx)
+        {
+            for (int i = _startIdx; i < _endIdx; i++)
+            {
+                if (_gameObjects[i].transform.localPosition.y == _originY && _gameObjects[i].GetComponent<MeshRenderer>().enabled == false)
+                {
+                    _gameObjects[i].GetComponent<MeshRenderer>().enabled = true;
+                }
+                if (_gameObjects[_endIdx - 1].transform.localPosition.y > y)
+                {
+                    if (_endIdx < _gameObjects.Length) _endIdx++;
+                }
+                if (_gameObjects[i].transform.localPosition.y < _endY) _gameObjects[i].transform.localPosition = new Vector3(_gameObjects[i].transform.localPosition.x, _gameObjects[i].transform.localPosition.y + 1, _gameObjects[i].transform.localPosition.z);
+                else if (_gameObjects[i].transform.localPosition.y >= _endY)
+                    _startIdx = i + 1;
+            }
+            if (_startIdx == _endIdx)
+            {
+                _initEffect = false;
+                _endIdx = 1;
+                _startIdx = 0;
+                StartCoroutine(StartWait(_delayTime));
+            }
+        }
+        //Effect3
+        if (_rotateEffect == true && _startIdx != _endIdx)
+        {
+            for (int i = _startIdx; i < _endIdx; i++)
+            {
+                if (_gameObjects[_endIdx - 1].transform.localEulerAngles.x > _changeDegrees)
+                {
+                    if (_endIdx < _gameObjects.Length) _endIdx++;
+                }
+
+                if (_gameObjects[i].transform.localEulerAngles.x <= 360)
+                {
+                    _gameObjects[i].transform.Rotate(Vector3.right, _degrees);
+                    _count[i]++;
+                    if (_count[i] == 360 / (int)_degrees) _startIdx = i + 1;
+                }
+            }
+            if (_startIdx == _endIdx)
+            {
+                _rotateEffect = false;
+                _isStartRotate = false;
+                _endIdx = 1;
+                _startIdx = 0;
+                StartCoroutine(WaitForShowEffect(2F));
+            }
+        }
+    }
+    public void StartRotate()
+    {
+        _isStartRotate = true;
+        StartCoroutine(WaitForRotateEffect(2F));
+    }
+    IEnumerator StartWait(float time)
+    {
+        yield return StartCoroutine(Wait(time));
+        _gameObjects[_idx].GetComponent<Effect1>()._isChanged = true;
+        _gameObjects[_idx].GetComponent<Effect1>()._type = 0;
+        _gameObjects[_idx].GetComponent<MeshRenderer>().material.color = new Color((float)R/255, (float)G /255, (float)B /255);
+        _idx++;
+        if (_idx != _gameObjects.Length) StartCoroutine(StartWait(_delayTime));
+        else
+        {
+            for (int i = 0; i < _gameObjects.Length; i++)
+            {
+                _gameObjects[i].GetComponent<Effect1>()._isShow = true;
+                _gameObjects[i].transform.FindChild("ETF_Landmine").GetComponent<ParticleSystem>().Play(true);
+            }
+            gameObject.GetComponent<Effect4>()._isShow = true;
+        }
+    }
+
+    IEnumerator WaitForRotateEffect(float time)
+    {
+        yield return StartCoroutine(Wait(time));
+        _rotateEffect = true;
+    }
+    IEnumerator WaitForShowEffect(float time)
+    {
+        yield return StartCoroutine(Wait(time));
+        //Show text
+        for (int i = 0; i < _gameObjects.Length; i++)
+        {
+            if (_gameObjects[i].transform.FindChild("Name") != null) _gameObjects[i].transform.FindChild("Name").GetComponent<MeshRenderer>().enabled = true;
+            if (_gameObjects[i].transform.FindChild("Price") != null) _gameObjects[i].transform.FindChild("Price").GetComponent<MeshRenderer>().enabled = true;
+            if (_gameObjects[i].transform.FindChild("Type") != null) _gameObjects[i].transform.FindChild("Type").GetComponent<MeshRenderer>().enabled = true;
+            _gameObjects[i].transform.FindChild("explode").GetComponent<ParticleSystem>().Play(true);
+        }
+    }
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+}
---
+++ ARFinalProject/Assets/Scripts/Games/Roulette/Roulette.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Roulette : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject _wheel;
+    public Material _lightOn;
+    public Material _lightOff;
+    private GameObject[] _bulbs;
+    private GameObject _vButton;
+    private int[] _values;
+
+    // Use this for initialization
+    void Start()
+    {
+        _vButton = GameObject.Find("VirtualButton");
+        _values = new int[] { 0, -26, 3, -35, 12, -28, 7, -29, 18, -22, 9, -31, 14, -20, 1, -33, 16, -24, 5, -10, 23, -8, 30, -11, 36, -13, 27, -6, 34, -17, 25, -2, 21, -4, 19, -15, 32 };
+        _bulbs = GameObject.FindGameObjectsWithTag("Bulb");
+        StartCoroutine(LightUp(1F));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Bulb Show
+
+    }
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        //TO DO: Implement virtual button press handler
+    }
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+
+    }
+    public void onClick()
+    {
+        _wheel.GetComponent<WheelRotate>()._speed = Random.Range(200, 400);
+        _wheel.GetComponent<WheelRotate>()._isStart = true;
+        StartCoroutine(StopPot(8F));
+    }
+    IEnumerator StopPot(float time)
+    {
+        yield return StartCoroutine(Wait(time));
+
+        _wheel.GetComponent<WheelRotate>()._speed -= 1;
+        if (_wheel.GetComponent<WheelRotate>()._speed > 0) StartCoroutine(StopPot(0.1F));
+        else
+        {
+            if (_wheel.transform.rotation.eulerAngles.y >= 265.587) Debug.Log(_values[(int)((_wheel.transform.rotation.eulerAngles.y - 265.587) / ((float)360 / 37))]);
+            else Debug.Log(_values[(int)((_wheel.transform.rotation.eulerAngles.y + 360 - 265.587) / ((float)360 / 37))]);
+        }
+        //Stop pot and continue counting until 3 pots are stopped
+    }
+    IEnumerator LightUp(float time)
+    {
+        yield return StartCoroutine(Wait(time));
+        for (int i = 0; i < 13; i++)
+        {
+            if (_bulbs[i].GetComponent<MeshRenderer>().material.color == _lightOn.color) _bulbs[i].GetComponent<MeshRenderer>().material.color = _lightOff.color;
+            else _bulbs[i].GetComponent<MeshRenderer>().material.color = _lightOn.color;
+        }
+        StartCoroutine(LightUp(1F));
+    }
+        IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+}
